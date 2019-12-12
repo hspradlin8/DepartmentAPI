b99942fb519f7934dcc146c5657551e6af3c6563
Heidi <hspradlin8@gmail.com>
2019-12-12 15:45:32 -0600

    API Commit

---
+++ Model/Department.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DepartmentAPI.Model
+{
+    public class Department
+    {
+        public string DeptName { get; set; }
+        public int Id { get; set; }
+    }
+}
---
+++ Model/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DepartmentAPI.Controllers
+{
+    public class Employee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Id { get; set; }
+        public int DepartmentId { get; set; }
+    }
+}
